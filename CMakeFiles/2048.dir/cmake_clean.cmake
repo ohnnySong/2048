93652f437410b67646d4e9b970741d4a68857ba2
ohnnySong <Yunxiang83@outlook.com>
2024-04-24 15:46:47 +0800

    resolution

---
+++ CMakeFiles/2048.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/yunxiang/Desktop/twentyFour/executable/2048"
+  "/Users/yunxiang/Desktop/twentyFour/executable/2048.pdb"
+  "CMakeFiles/2048.dir/2048game.c.o"
+  "CMakeFiles/2048.dir/2048game.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2048.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
